e3da5305878b396d6b66c60d0f9718a20f1d70c1
FabianLi2002 <bamboorat233@gmail.com>
2025-10-20 11:50:58 -0400

    update Db conn

--- BookStoreLIB/Properties/Settings.Designer.cs
+++ BookStoreLIB/Properties/Settings.Designer.cs
@@ -26,8 +26,9 @@ namespace BookStoreLIB.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\26773\\source\\repos\\k" +
-            "LBookStore\\xyBookstoreDB.mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\26773\\source\\repos\\F" +
+            "b_SL_BookStore\\kLBookStore\\xyBookstoreDB.mdf;Integrated Security=True;Connect Ti" +
+            "meout=30")]
         public string XYConn {
             get {
                 return ((string)(this["XYConn"]));
